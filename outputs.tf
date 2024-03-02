800101b0a211ece5688b561b2280ac2dfb82bb67
Victor Galvez <vhgalvez@gmail.com>
2024-03-03 00:48:13 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,4 @@
 # terraform\outputs.tf
 output "ip-addresses" {
-   value = {
-     for key, machine in libvirt_domain.machine : key => can(index(machine.network_interface, 0)) ? machine.network_interface[0].addresses[0] : null
-   }
+  value = { for key in var.machines : key => libvirt_domain.machine[key].network_interface.0.addresses[0] }
 }
