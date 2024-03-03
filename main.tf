c3570275db2f92e53906b088becae9aa761ea125
Victor Galvez <vhgalvez@gmail.com>
2024-03-03 01:06:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,8 @@ resource "libvirt_volume" "base" {
 data "ct_config" "ignition" {
   for_each = toset(var.machines)
   content  = templatefile("${path.module}/configs/${each.key}-config.yaml.tmpl", {
-    ssh_keys = var.ssh_keys
+    ssh_keys = var.ssh_keys,
+    message = "tu mensaje aquí"
   })
 }
 
