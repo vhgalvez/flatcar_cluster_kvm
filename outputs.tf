857ee5cbe45ccf9461eaa1d40e415c6441851747
Victor Galvez <vhgalvez@gmail.com>
2024-03-03 02:03:32 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,4 @@
 # terraform\outputs.tfoutput "ip-addresses" {
-  value = {
-    for key in var.machines : 
-    key => try(libvirt_domain.machine[key].network_interface.0.addresses[0], "IP not assigned")
-  }
+  output "ip-addresses" {
+  value = { for key in var.machines : key => try(libvirt_domain.machine[key].network_interface.0.addresses[0], "IP not assigned") }
 }
