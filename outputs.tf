ca0a7cecc4624223a00a02496c8258fe8445e733
Victor Galvez <vhgalvez@gmail.com>
2024-03-03 01:19:28 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 # terraform\outputs.tf
 output "ip-addresses" {
-  value = { for key in var.machines : key => libvirt_domain.machine[key].network_interface.0.addresses[0] }
+  value = { for key in var.machines : key =>
+    length(libvirt_domain.machine[key].network_interface) > 0 &&
+    length(libvirt_domain.machine[key].network_interface.0.addresses) > 0 ?
+    libvirt_domain.machine[key].network_interface.0.addresses[0] :
+    "No IP address found"
+  }
 }
