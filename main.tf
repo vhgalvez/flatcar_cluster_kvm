9f1b33a33563815a6a1500c04bbb5dff70d5379d
Victor Galvez <vhgalvez@gmail.com>
2024-03-08 00:59:47 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "libvirt_volume" "base" {
 
 data "template_file" "vm-configs" {
   for_each = toset(var.machines)
-  template = file("${path.module}/machine-${each.key}.yaml.tmpl")
+  template = file("${path.module}/configs/${each.key}-config.yaml.tmpl",)
 
   vars = {
     ssh_keys = jsonencode(var.ssh_keys)
