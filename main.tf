2eae94174e3b4d3a74af3e38dcab83384af42254
Victor Galvez <vhgalvez@gmail.com>
2024-03-07 05:30:41 +0100

    up

--- main.tf
+++ main.tf
@@ -39,13 +39,16 @@ resource "libvirt_volume" "base" {
 }
 
 
+
+
 data "ct_config" "ignition" {
   for_each = toset(var.machines)
 
-  content = templatefile("${path.module}/configs/${each.value}-config.yaml.tmpl", {
-    ssh_authorized_keys = jsonencode(var.ssh_keys),
-    message             = "Welcome to Flatcar Linux!"
+  content = templatefile("${path.module}/configs/${each.key}-config.yaml.tmpl", {
+    ssh_keys = jsonencode(var.ssh_keys), // Make sure this matches the variable expected in your template
+    message  = "Welcome to Flatcar Linux!"
   })
+
   strict       = true
   pretty_print = true
 }
