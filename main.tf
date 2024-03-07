81b87387108cece508197687f1426122d9ee9659
Victor Galvez <vhgalvez@gmail.com>
2024-03-07 01:45:48 +0100

    update

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "libvirt_volume" "base" {
 data "ct_config" "ignition" {
   for_each = toset(var.machines)
   content = templatefile("${path.module}/configs/${each.key}-config.yaml.tmpl", {
-    ssh_keys = var.ssh_keys,
+    ssh_keys = join("\n", var.ssh_keys),
     message  = "Welcome to Flatcar Linux"
   })
 }
