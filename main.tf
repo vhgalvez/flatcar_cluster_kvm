4bbe9269fb78912f7322662a60388003b4e5bd64
Victor Galvez <vhgalvez@gmail.com>
2024-03-07 04:42:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "libvirt_volume" "base" {
 
 
 data "ct_config" "example" {
-  content = templatefile("${path.module}/path/to/your/template.yaml", {
+  content = templatefile("${path.module}/configs/${each.key}-config.yaml.tmpl", {
     ssh_keys = join("\n  - ", var.ssh_keys), # Esto unirá la lista con una nueva línea y guiones como prefijos
     message  = "Este es tu mensaje personalizado"
   })
